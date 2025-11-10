0a3e00a64f119d913bc7c111560a830abf10700f
komal.nerpagar <nerkomal81@gmail.com>
2025-11-10 17:20:21 +0530

    PANKAJ COMMITED

---
+++ src/main/java/Page_layer/PIM_Page.java
@@ -0,0 +1,20 @@
+package Page_layer;
+
+public class PIM_Page {
+
+	public static void main(String[]args) {
+		
+		
+		
+		System.out.println("PANKAJ WORKSPACE");
+		
+		
+		
+		
+		
+		
+		
+	}
+	
+	
+}
