2d4a8f23475cbae364eddf9b64b12366b590dce3
komal.nerpagar <nerkomal81@gmail.com>
2025-11-10 17:04:38 +0530

    KOMAL SELENIUM PROJECT COMMITED

---
+++ src/main/java/Page_layer/login_page.java
@@ -0,0 +1,20 @@
+package Page_layer;
+
+public class login_page {
+	
+	public static void main(String[] args) {
+		
+	
+ 
+	System.out.println("komal as Team leader");
+	
+	
+	
+	
+	
+	}
+	
+	
+	
+	
+}
