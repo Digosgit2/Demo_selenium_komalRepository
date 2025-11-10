c36b06e9ae3bec0ee4f9f984fd8811f0869e38ee
komal.nerpagar <nerkomal81@gmail.com>
2025-11-10 17:27:36 +0530

    Nikhil commited

---
+++ src/main/java/Page_layer/Personal_Details_Page.java
@@ -0,0 +1,17 @@
+package Page_layer;
+
+public class Personal_Details_Page {
+
+	public static void main(String[] args) {
+
+		
+		System.out.println("nikhil page");
+		
+		
+		
+		
+		
+		
+	}
+
+}
